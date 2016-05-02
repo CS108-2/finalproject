447314f8e61a24bfba33c5c93715d92b79cd46f4
229KyleSE <229kyleescott@gmail.com>
2016-05-02 15:02:25 -0700

    Update Player.cs

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,12 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public enum player_type {
+		ai,
+		player_1,
+		player_2
+	}
+
+	public player_type player;
+
 	public int turning_speed;
+
 	public int speed;
 	public float turning;
 	public float velocity;
+	public float MAX_VELOCITY = 100;
 	public Vector2 movement;
 
 	public GameObject bullet;
@@ -29,11 +39,11 @@ public class Player : MonoBehaviour {
 
 		rbody = gameObject.GetComponent<Rigidbody2D> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		turning = Input.GetAxis ("Horizontal");
-		velocity = Input.GetAxis ("Vertical");
+		turning = GetTurning ();
+		velocity = GetVelocity ();
 
 		bullettime += Time.deltaTime;
 
@@ -44,10 +54,44 @@ public class Player : MonoBehaviour {
 		}
 	}
 
+	float GetTurning() {
+		if (player == player_type.player_1)
+			return Input.GetAxis ("Horizontal");
+		else if (player == player_type.player_2)
+			return Input.GetAxis ("Horizontal2");
+		// or something like this, not yet sure how we should implement AI. Maybe as a separate class?
+//		else if (player == players.ai)
+//			return AI.GetTurning()
+		else return 0; // error
+	}
+
+	float GetVelocity() {
+		if (player == player_type.player_1)
+			return Input.GetAxis ("Vertical");
+		else if (player == player_type.player_2)
+			return Input.GetAxis ("Vertical2");
+		// or something like this, not yet sure how we should implement AI. Maybe as a separate class?
+//		else if (player == players.ai)
+//			return AI.GetTurning()
+		else return 0; // error
+	}
+
 	void FixedUpdate() {
 		if (velocity >= 0)
 			turning *= -1;
 		transform.Rotate(0.0f, 0.0f, turning * Time.deltaTime * turning_speed);
 		rbody.velocity = this.transform.up * velocity * Time.deltaTime * speed;
+
+		if (this.hp <= 0)
+			Destroy (this.gameObject);
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.tag == "damaging"){
+			int damageGiven;
+			damageGiven = (int)Mathf.Round ((Mathf.Abs(this.velocity)) * 20);
+			other.gameObject.GetComponent<Player> ().hp -= damageGiven;
+		}
 	}
 }
