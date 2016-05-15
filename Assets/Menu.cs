3c97aea9f72234b20e9e528ac1cdd199811e96db
Johnathon Ludeman <john@ludeman.com>
2016-05-14 23:42:42 -0700

    created main menu scene

---
+++ Assets/Menu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	private GameObject menu;
+	private GameObject controls;
+
+	// Use this for initialization
+	void Start () {
+		menu = GameObject.Find ("Menu");
+		controls = GameObject.Find ("Controls");
+		menu.SetActive (true);
+		controls.SetActive (false);
+	}
+
+	public void Play() {
+		SceneManager.LoadScene ("game");
+	}
+
+	public void Quit() {
+		Application.Quit ();
+	}
+
+	public void showMenu() {
+		menu.SetActive (true);
+		controls.SetActive (false);
+	}
+
+	public void showControls() {
+		menu.SetActive (false);
+		controls.SetActive (true);
+	}
+}
