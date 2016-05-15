aa015f285324a9ee82e648fca30175c521bd5d24
Johnathon Ludeman <john@ludeman.com>
2016-05-14 22:42:07 -0700

    add shield powerup

---
+++ Assets/ShieldPickup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldPickup : Powerup {
+
+	public GameObject shield;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// to be overridden by subclasses
+	override protected void AddPowerup(Player player) {
+		player.shield = true;
+		Shield shield = (Shield)(Instantiate (this.shield, player.transform.position, player.transform.rotation) as GameObject).GetComponent<Shield>();
+		shield.transform.parent = player.transform;
+		shield.player = player;
+	}
+}
